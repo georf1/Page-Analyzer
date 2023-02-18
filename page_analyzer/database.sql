cfca1e2a6c9c0fc24db72227796a1b2d28ff5e42
Ruslan Zinnatov <rh26157@gmail.com>
2023-02-18 17:32:05 +0300

    update database.sql

--- page_analyzer/database.sql
+++ page_analyzer/database.sql
@@ -1,6 +1,16 @@
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS urls;
+DROP TABLE IF EXISTS url_checks;
 CREATE TABLE urls (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  name VARCHAR(255) UNIQUE NOT NULL,
-  created_at TIMESTAMP NOT NULL
+  name varchar(255) UNIQUE NOT NULL,
+  created_at date NOT NULL
+);
+CREATE TABLE url_checks (
+  id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  url_id bigint REFERENCES urls(id) NOT NULL,
+  status_code int,
+  h1 varchar(255),
+  title varchar(255),
+  description varchar(255),
+  created_at date NOT NULL
 );
